a3553fd0bfe1fa9a5a81cd9e18582f8f03936096
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2019-09-25 20:39:25 -0600

    cleanup

--- leafWRFtools/mapplot.py
+++
@@ -1,8 +0,0 @@
-from MultiNC import *
-
-sample_wrf_file = "/home/wrudisill/scratch/WRF_PROJECTS/wrf_cfsr_1998-02-01_00__1998-03-03_00/wrf_out/wrfout_d02_1998-03-01_00:00:00"
-
-ds = xr.open_mfdataset("../Data/AR96_NoSpinUp.nc")
-t1 = ds['FWALL'][0:75,:,:].sum(axis=0)/(75*60*60)
-t1.to_netcdf("fwall.tmp")
-NCL(FNAME="fwall.tmp", VARNAME="FWALL", WRF_EXAMPLE_FILE=sample_wrf_file, PLOTTITLE="no_spin", UNITS="W/m2")
